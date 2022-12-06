42a1adeec0adc209ecc1801abaa6c782f70885d6
Felix Schmidt <78191260+buildersinc@users.noreply.github.com>
2022-12-06 21:19:21 +0100

    Fix chatmessages
    (cherry picked from commit 3ff4cf788ffddff1a194acf3efb0bdbda4e92ce0)

--- src/main/java/de/afgmedia/ftspest/misc/Values.java
+++ src/main/java/de/afgmedia/ftspest/misc/Values.java
@@ -1,21 +1,14 @@
 package de.afgmedia.ftspest.misc;
 
+import org.bukkit.ChatColor;
+
 public class Values {
 
-    public static final String PREFIX = "§7[§bFTS-Pest§7] ";
+    public static final String PREFIX = ChatColor.GRAY + "[" + ChatColor.BOLD  + "FTS-Pest" + ChatColor.GRAY  + "] ";
 
-    public static final String MESSAGE_HEALED = "§7[§bFTS-Pest§7] Du wurdest geheilt!";
+    public static final String MESSAGE_HEALED = PREFIX + "Du wurdest geheilt!";
 
-    public static final String MESSAGE_WRONG_MEDICINE = "§7[§bFTS-Pest§7] Du legst die Medizin wieder weg als du bemerkst dass es nicht die ist, die du brauchst";
-
-    public static final String
-            NAME_ITEM_VACCINE = "§6Impfung",
-            NAME_ITEM_SERUM = "§6Serum",
-            NAME_ITEM_SYRINGE = "§6Spritze",
-            NAME_ITEM_SYRINGE_WITH_BLOOD = "§6Spritze mit Blut";
-
-    public static final String LORE_IDENTIFIER = " F T S I T E M".replace(" ", "§");
-
+    public static final String MESSAGE_WRONG_MEDICINE = PREFIX + "Du legst die Medizin wieder weg als du bemerkst dass es nicht die ist, die du brauchst";
 }
 
 
